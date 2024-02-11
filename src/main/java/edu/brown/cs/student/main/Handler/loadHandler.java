298b2323553c005383c6d9fa672176874c44d102
Nick Li <nick@nicks-mbp-10.devices.brown.edu>
2024-02-11 18:42:19 -0500

    adding data folder for csv files

--- src/main/java/edu/brown/cs/student/main/Handler/loadHandler.java
+++ src/main/java/edu/brown/cs/student/main/Handler/loadHandler.java
@@ -1,5 +1,6 @@
 package edu.brown.cs.student.main.Handler;
 
+import java.io.FileReader;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -9,7 +10,9 @@ public class loadHandler implements Route {
   @Override
   public Object handle(Request request, Response response) throws Exception {
     String filepath = request.queryParams("filepath");
+    try (FileReader reader = new FileReader()) {
 
+    }
     return null;
   }
 }
