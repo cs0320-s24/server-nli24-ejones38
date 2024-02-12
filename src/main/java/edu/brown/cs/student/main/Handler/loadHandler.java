bf2462d3afc0283b8aa1d3a4bd9b1b91d93aa40c
Nick Li <nick@nicks-mbp-10.devices.brown.edu>
2024-02-11 20:07:01 -0500

    test load

--- src/main/java/edu/brown/cs/student/main/Handler/loadHandler.java
+++ src/main/java/edu/brown/cs/student/main/Handler/loadHandler.java
@@ -1,6 +1,13 @@
 package edu.brown.cs.student.main.Handler;
 
+import com.squareup.moshi.JsonAdapter;
+import com.squareup.moshi.Moshi;
+import com.squareup.moshi.Types;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -9,10 +16,24 @@ public class loadHandler implements Route {
 
   @Override
   public Object handle(Request request, Response response) throws Exception {
-    String filepath = request.queryParams("filepath");
-    try (FileReader reader = new FileReader()) {
-
+    String filePath = request.queryParams("filepath");
+    Map<String,Object> responseMap = new HashMap<>();
+    Moshi moshi = new Moshi.Builder().build();
+    Type mapStringObject = Types.newParameterizedType(Map.class, String.class, Object.class);
+    JsonAdapter<Map<String, Object>> adapter = moshi.adapter(mapStringObject);
+    try (FileReader reader = new FileReader("/Users/nick/Desktop/CS32/Sprints/server-nli24-ejones38/data/census/" + filePath)) {
+      responseMap.put("filepath", filePath);
+      responseMap.put("result", "success");
+      return adapter.toJson(responseMap);
     }
-    return null;
+    catch(FileNotFoundException e) {
+      responseMap.put("filepath", filePath);
+      responseMap.put("result", "error");
+      responseMap.put("error_type", "datasource");
+      responseMap.put("details", e.getMessage()); //dk if e.getMessage is correct
+      return adapter.toJson(responseMap);
+    }
+
+
   }
 }
--- src/main/java/edu/brown/cs/student/main/Server.java
+++ src/main/java/edu/brown/cs/student/main/Server.java
@@ -1,7 +1,9 @@
 package edu.brown.cs.student.main;
 import static spark.Spark.after;
 
+import edu.brown.cs.student.main.Handler.loadHandler;
 import edu.brown.cs.student.main.Handler.searchHandler;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 import spark.Spark;
@@ -14,33 +16,29 @@ import spark.Spark;
  * injection. If we needed more endpoints, more functionality classes, etc. we could make sure they
  * all had the same shared state.
  */
-public class Server {
 
-  public static void main(String[] args) {
+public class Server {
+  private Reader state; //dont know if reader object is correct shared state
+  public Server() {
+//    this.state = loadValidity;
     int port = 3232;
     Spark.port(port);
-    /*
-       Setting CORS headers to allow cross-origin requests from the client; this is necessary for the client to
-       be able to make requests to the server.
-
-       By setting the Access-Control-Allow-Origin header to "*", we allow requests from any origin.
-       This is not a good idea in real-world applications, since it opens up your server to cross-origin requests
-       from any website. Instead, you should set this header to the origin of your client, or a list of origins
-       that you trust.
-
-       By setting the Access-Control-Allow-Methods header to "*", we allow requests with any HTTP method.
-       Again, it's generally better to be more specific here and only allow the methods you need, but for
-       this demo we'll allow all methods.
-
-       We recommend you learn more about CORS with these resources:
-           - https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-           - https://portswigger.net/web-security/cors
-    */
     after(
         (request, response) -> {
           response.header("Access-Control-Allow-Origin", "*");
           response.header("Access-Control-Allow-Methods", "*");
         });
+    Spark.get("loadcsv", new loadHandler());
+    Spark.get("searchcsv", new searchHandler());
+    Spark.init();
+    Spark.awaitInitialization();
+
+    // Notice this link alone leads to a 404... Why is that?
+    System.out.println("Server started at http://localhost:" + port);
+  }
+
+  public static void main(String[] args) {
+      Server server = new Server();
 
     // Sets up data needed for the OrderHandler. You will likely not read from local
     // JSON in this sprint.
@@ -56,12 +54,7 @@ public class Server {
 //    }
 
     // Setting up the handler for the GET /order and /activity endpoints
-    Spark.get("searchcsv", new searchHandler());
-    Spark.init();
-    Spark.awaitInitialization();
 
-    // Notice this link alone leads to a 404... Why is that?
-    System.out.println("Server started at http://localhost:" + port);
   }
 
 
